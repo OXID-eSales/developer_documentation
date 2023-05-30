298bcc398fc72f577e1cdaca8d21c5312503e49b
christoph albrecht <christoph.albrecht@oxid-esales.com>
2023-05-30 17:22:23 +0200

    6.4.x version switch adjusted to 700

--- conf.py
+++ conf.py
@@ -131,11 +131,12 @@ html_theme = 'sphinx_rtd_theme'
 html_context = {
     'current_version': '6.4',
     'versions':
-        [('6.1', 'https://docs.oxid-esales.com/developer/en/6.1/'),
+        [
          ('6.2', 'https://docs.oxid-esales.com/developer/en/6.2/'),
          ('6.3', 'https://docs.oxid-esales.com/developer/en/6.3/'),
          ('6.4', 'https://docs.oxid-esales.com/developer/en/6.4/'),
-         ('6.5', 'https://docs.oxid-esales.com/developer/en/6.5/')],
+         ('6.5', 'https://docs.oxid-esales.com/developer/en/6.5/'),
+         ('7.0', 'https://docs.oxid-esales.com/developer/en/7.0/'),],
     'css_files': [('_static/oxid.css')],
     'theme_logo_only': True,
     'show_sphinx': False,
