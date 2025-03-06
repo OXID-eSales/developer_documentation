042102aa6111e6f129d3febb3ce6928f4adbb47e
christoph albrecht <christoph.albrecht@oxid-esales.com>
2025-03-06 09:01:17 +0100

    OXDEV-8922: merge 7.2, adjust conf.py

--- conf.py
+++ conf.py
@@ -133,7 +133,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 html_context = {
-    'current_version': '7.2',
+    'current_version': '7.3',
     'versions':
         [
          ('6.2', 'https://docs.oxid-esales.com/developer/en/6.2/'),
@@ -143,6 +143,7 @@ html_context = {
          ('7.0', 'https://docs.oxid-esales.com/developer/en/7.0/'),
          ('7.1', 'https://docs.oxid-esales.com/developer/en/7.1/'),
          ('7.2', 'https://docs.oxid-esales.com/developer/en/7.2/'),
+         ('7.3', 'https://docs.oxid-esales.com/developer/en/7.3/'),
         ],
     'css_files': [('_static/oxid.css')],
     'theme_logo_only': True,
