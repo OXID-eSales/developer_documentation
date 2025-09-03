1dc9b07d610dbc529a8d841733b190d651465f31
Steven <57273424+oxSteven@users.noreply.github.com>
2025-09-03 14:06:33 +0200

    OXDEV-9739: Added version 7.3 to nav.

--- conf.py
+++ conf.py
@@ -141,6 +141,7 @@ html_context = {
          ('7.0', 'https://docs.oxid-esales.com/developer/en/7.0/'),
          ('7.1', 'https://docs.oxid-esales.com/developer/en/7.1/'),
          ('7.2', 'https://docs.oxid-esales.com/developer/en/7.2/'),
+         ('7.3', 'https://docs.oxid-esales.com/developer/en/7.3/'),
         ],
     'css_files': [('_static/oxid.css')],
     'theme_logo_only': True,
